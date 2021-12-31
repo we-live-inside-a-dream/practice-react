83c8c2cb049519769033835c84cafb7e0cf6a554
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2021-12-31 10:58:10 -0700

    created a Post method using firebase

--- src/components/layout/Meetups/NewMeetupForm.js
+++ src/components/layout/Meetups/NewMeetupForm.js
@@ -3,7 +3,7 @@ import { useRef } from "react"
 import Card from "../ui/Card"
 import classes from './NewMeetupForm.module.css'
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
     const titleInputRef = useRef()
     const imageInputRef = useRef()
     const addressInputRef = useRef()
@@ -23,7 +23,7 @@ function NewMeetupForm() {
             address: enteredAddress,
             description: enteredDescription
         }
-        console.log(meetupData)
+        props.onAddMeetup(meetupData)
     }
 
     return <Card>
