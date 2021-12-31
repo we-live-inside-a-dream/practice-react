c1f411c27c756e8e6764ff4ab2ee46663494ef8e
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2021-12-31 10:57:53 -0700

    created a server POST using firebase

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -2,9 +2,22 @@ import NewMeetupForm from "../components/layout/Meetups/NewMeetupForm"
 
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetupData){
+        fetch(
+            'https://react-practice-tutorial2-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST', 
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+    }
+
     return <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
     
 }
